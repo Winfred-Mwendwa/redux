99c81dcdb99794d22b4556ce95ece3dcb6641e69
winfred mwendwa <mwendwawinfred8@gmail.com>
2024-01-14 17:42:24 +0300

    [skip-ci]initial setup

---
+++ redux-state/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <h2>Redux Complete Tutorial</h2>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
